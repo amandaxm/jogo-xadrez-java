73f08c2c0d2d0f14f13ef72970a414de64688fa5
Amanda Xavier <amandaxaviermariano10@gmail.com>
2020-05-15 15:08:04 -0300

    Iniciando Tabuleiro e Peça

---
+++ src/tabuleiro/Tabuleiro.java
@@ -0,0 +1,32 @@
+package tabuleiro;
+
+public class Tabuleiro {
+	public int linhas;
+	public int colunas;
+	// tera uma matriz de peças
+
+	private Piece[][] pecas;
+
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		pecas = new Piece[linhas][colunas];
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+
+}
