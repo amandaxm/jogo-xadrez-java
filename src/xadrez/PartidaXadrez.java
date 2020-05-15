f0c39b5d1f72dc34373414656c4e97cc05cd16dd
Amanda Xavier <amandaxaviermariano10@gmail.com>
2020-05-15 20:14:31 -0300

    Excecao Xadrez

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -6,33 +6,40 @@ import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 
 public class PartidaXadrez {
- //coração do jogo
+	// coração do jogo
 	private Tabuleiro tabuleiro;
+
 	public PartidaXadrez() {
-		//dizer o tamanho
-		//crio o tabuleiro e inicia o jogo
-		tabuleiro = new Tabuleiro(8,8);
+		// dizer o tamanho
+		// crio o tabuleiro e inicia o jogo
+		tabuleiro = new Tabuleiro(8, 8);
 		iniciarJogo();
 	}
-	//retornar uma matriz de peças de xadrez
-	//correspondente a essa partida
-	public PecaXadrez[][] getPecas(){
-		// o tabuleiro tem as pecas
-	PecaXadrez [][] matriz = new PecaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];	
-	//percorrer a matriz de pecas do tabuleiro 	
-	for (int i =0;i<tabuleiro.getLinhas();i++) {
-		for (int j =0;j<tabuleiro.getColunas();j++) {
-			matriz[i][j] = (PecaXadrez) tabuleiro.piece(i,j);
-			
-		}
-	}
-	return matriz;
-	}
-	private void iniciarJogo() {
-		tabuleiro.lugarPeca(new Torre(tabuleiro, Color.WHITE), new Posicao(2,1));
-		tabuleiro.lugarPeca(new Rei(tabuleiro, Color.BLACK), new Posicao(0,4));
-		tabuleiro.lugarPeca(new Rei(tabuleiro, Color.BLACK), new Posicao(7,4));
 
-		
+	// retornar uma matriz de peças de xadrez
+	// correspondente a essa partida
+	public PecaXadrez[][] getPecas() {
+		// o tabuleiro tem as pecas
+		PecaXadrez[][] matriz = new PecaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+		// percorrer a matriz de pecas do tabuleiro
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				matriz[i][j] = (PecaXadrez) tabuleiro.piece(i, j);
+
+			}
+		}
+		return matriz;
+	}
+
+	private void placeNewPiece(char coluna, int linha, PecaXadrez peca) {
+		tabuleiro.lugarPeca(peca, new PosicaoXadrez(coluna, linha).toPosition());
+
+	}
+
+	private void iniciarJogo() {
+		placeNewPiece('b', 6, new Torre(tabuleiro, Color.WHITE));
+		placeNewPiece('e', 8, new Rei(tabuleiro, Color.BLACK));
+		placeNewPiece('e', 1, new Rei(tabuleiro, Color.WHITE));
+
 	}
 }
---
+++ src/xadrez/PosicaoXadrez.java
@@ -0,0 +1,45 @@
+package xadrez;
+
+import tabuleiro.Posicao;
+
+public class PosicaoXadrez {
+	private char coluna;
+	private int linha;
+
+	public PosicaoXadrez(char coluna, int linha) {
+		if (coluna < 'a' || coluna > 'h' || linha < 1 || linha > 8) {
+
+			throw new ExcecaoXadrez("Erro, posicoes de a/h e 1/8");
+		}
+		this.coluna=coluna;
+		this.linha=linha;
+
+	}
+
+	public char getColuna() {
+		return coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	protected Posicao toPosition() {
+		// converte a b em posicao 5 6
+		return new Posicao(8 - linha, coluna - 'a');
+
+	}
+
+	protected static PosicaoXadrez paraPosicao(Posicao posicao) {
+
+		return new PosicaoXadrez((char) ('a' - posicao.getColuna()), 8 - posicao.getLinha());
+
+	}
+	@Override
+	public String toString() {
+		//imprimir coluna linha
+		return ""+ coluna+ linha;
+		
+	}
+
+}
