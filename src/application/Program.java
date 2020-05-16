f1982af140d3cccc98d33b36b4a810a4c3129c48
myusername <amandaxaviermariano10@gmail.com>
2020-05-16 15:22:38 -0300

    Limpando tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadrez.ExcecaoXadrez;
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
@@ -15,19 +17,28 @@ public class Program {
 		
 		//funcao para imprimir as pecas dessa partida
 		while(true) {
+		try {	
+			Interface.clearScreen();
+			Interface.imprimeTabuleiro(partidaXadrez.getPecas());
+			System.out.println();
+			System.out.println("Origem: ");
+			PosicaoXadrez origem = Interface.lerPosicao(sc);
 			
-		
-		Interface.imprimeTabuleiro(partidaXadrez.getPecas());
-		System.out.println();
-		System.out.println("Origem: ");
-		PosicaoXadrez origem = Interface.lerPosicao(sc);
-		
-		System.out.println();
-		System.out.println("Destino: ");
-		PosicaoXadrez destino = Interface.lerPosicao(sc);
-		
-		PecaXadrez capturarPeca = partidaXadrez.executarMovimento(origem, destino);
-		
+			System.out.println();
+			System.out.println("Destino: ");
+			PosicaoXadrez destino = Interface.lerPosicao(sc);
+			
+			PecaXadrez capturarPeca = partidaXadrez.executarMovimento(origem, destino);
+			
+		}catch(ExcecaoXadrez e){
+			System.out.println(e.getMessage());
+			sc.nextLine();
+			
+		}catch(InputMismatchException e){
+			System.out.println(e.getMessage());
+			sc.nextLine();//aguardar pessoa pressionar tecla enter
+			
+		}
 		}
 		}
 
