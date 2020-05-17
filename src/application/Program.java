a85924d79f7b5f7ecdb792a13c53e828a54349a3
myusername <amandaxaviermariano10@gmail.com>
2020-05-17 14:42:48 -0300

    Definindo movimentos do rei

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,38 +14,37 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		PartidaXadrez partidaXadrez = new PartidaXadrez();
-		
-		//funcao para imprimir as pecas dessa partida
-		while(true) {
-		try {	
-			Interface.clearScreen();
-			Interface.imprimeTabuleiro(partidaXadrez.getPecas());
-			System.out.println();
-			System.out.println("Origem: ");
-			PosicaoXadrez origem = Interface.lerPosicao(sc);
-			
-			boolean[][] possibleMoves = partidaXadrez.possibleMoves(origem);
-			Interface.clearScreen();
-			//colorindo as possiveis posicoes
-			Interface.imprimeTabuleiro(partidaXadrez.getPecas(),possibleMoves);
 
-			
-			System.out.println();
-			System.out.println("Destino: ");
-			PosicaoXadrez destino = Interface.lerPosicao(sc);
-			
-			PecaXadrez capturarPeca = partidaXadrez.executarMovimento(origem, destino);
-			
-		}catch(ExcecaoXadrez e){
-			System.out.println(e.getMessage());
-			sc.nextLine();
-			
-		}catch(InputMismatchException e){
-			System.out.println(e.getMessage());
-			sc.nextLine();//aguardar pessoa pressionar tecla enter
-			
+		// funcao para imprimir as pecas dessa partida
+		while (true) {
+			try {
+				Interface.clearScreen();
+				Interface.imprimeTabuleiro(partidaXadrez.getPecas());
+				System.out.println();
+				System.out.println("Origem: ");
+				PosicaoXadrez origem = Interface.lerPosicao(sc);
+
+				boolean[][] possibleMoves = partidaXadrez.possibleMoves(origem);
+				Interface.clearScreen();
+				// colorindo as possiveis posicoes
+				Interface.imprimeTabuleiro(partidaXadrez.getPecas(), possibleMoves);
+
+				System.out.println();
+				System.out.println("Destino: ");
+				PosicaoXadrez destino = Interface.lerPosicao(sc);
+
+				PecaXadrez capturarPeca = partidaXadrez.executarMovimento(origem, destino);
+
+			} catch (ExcecaoXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();// aguardar pessoa pressionar tecla enter
+
+			}
 		}
-		}
-		}
+	}
 
 }
--- src/peca/xadrez/Rei.java
+++ src/peca/xadrez/Rei.java
@@ -1,5 +1,6 @@
 package peca.xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Color;
 import xadrez.PecaXadrez;
@@ -17,10 +18,76 @@ public class Rei extends PecaXadrez {
 
 	}
 
+	private boolean podeMover(Posicao posicao) {
+		// se o rei pode mover para essa posicao
+		PecaXadrez p = (PecaXadrez) getTabuleiro().piece(posicao);
+		return p == null || p.getColor() != getColor();
+		//ou casa vazia ou peca outro time
+	}
+
 	@Override
 	public boolean[][] movimentosPossiveis() {
 
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		Posicao p = new Posicao(0, 0);
+
+		// para cima
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			// entao pode se mover para essa posicao
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// para baixo
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			// entao pode se mover para essa posicao
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//para esquerda
+		p.setValues(posicao.getLinha(), posicao.getColuna()-1);
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+		// entao pode se mover para essa posicao
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//para direita
+		p.setValues(posicao.getLinha(), posicao.getColuna()+1);
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+		// entao pode se mover para essa posicao
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//noroeste
+		p.setValues(posicao.getLinha()-1, posicao.getColuna()-1);
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+		// entao pode se mover para essa posicao
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//nordeste
+		p.setValues(posicao.getLinha()-1, posicao.getColuna()+1);
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+		// entao pode se mover para essa posicao
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//sudoeste
+
+		p.setValues(posicao.getLinha()+1, posicao.getColuna()-1);
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+		// entao pode se mover para essa posicao
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//sudeste
+
+		p.setValues(posicao.getLinha()+1, posicao.getColuna()+1);
+		if (getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+		// entao pode se mover para essa posicao
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+
 
 		// TODO Auto-generated method stub
 		return mat;
